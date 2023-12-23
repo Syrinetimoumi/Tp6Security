ad0d3fe21e06eb6252fdba4cd63c80fe6608d53e
syrine timoumi <timoumisyrine@gmail.com>
2023-12-23 16:11:40 +0100

    first commit

---
+++ tpsecurity/src/main/java/com/example/tpsecurity/TpsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpsecurityApplication.class, args);
+	}
+
+}
